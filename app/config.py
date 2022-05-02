33c75f3f7c2b583be747f4c95a3700ca22d672d1
edah-hub <edah.chepngetich@student.moringaschool.comm>
2022-05-02 15:04:21 +0300

    imports devconfig sub class from config options

--- app/config.py
+++ app/config.py
@@ -2,7 +2,10 @@ class Config:
     '''
     General configuration parent class
     '''
-    pass
+    NEWS_HIGHLIGHTS_BASE_URL = 'https://newsapi.org/v2/top-headlines?country={}&apiKey={}'
+    NEWS_SOURCE_BASE_URL = 'https://newsapi.org/v2/sources?apiKey={}'
+    NEWS_ENTERTAINMENT_BASE_URL = 'https://newsapi.org/v2/top-headlines?category={}&apiKey={}'
+
 
 
 
@@ -24,4 +27,9 @@ class DevConfig(Config):
         Config: The parent configuration class with General configuration settings
     '''
 
-    DEBUG = True
+    DEBUG = True # debug mode
+    
+config_options = {
+    'development': DevConfig,
+    'production': ProdConfig
+}
