79eb077461a381456ccbcbd97c5e946f74297698
edah-hub <edah.chepngetich@student.moringaschool.comm>
2022-05-02 15:21:51 +0300

    instantiates bootsrap

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,8 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
 from .config import DevConfig
+
+bootstrap = Bootstrap()
 
 # Initializing application
 app = Flask(__name__,instance_relative_config = True)
