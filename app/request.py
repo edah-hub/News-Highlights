940f9af712ca69ea7795660bbaa9ee248baf8230
edah-hub <edah.chepngetich@student.moringaschool.comm>
2022-05-02 16:02:29 +0300

    calls app.config['name_of_object']

--- app/request.py
+++ app/request.py
@@ -1,4 +1,20 @@
 from app import app
+import datetime
+import urllib.request,json
+from .models import news,sources,entertainment
+
+News = news.News
+Sources = sources.Sources
+Entertainment = entertainment.Entertainment
 
 # Getting api key
 api_key = app.config['NEWS_API_KEY']
+
+# Getting the news base url
+news_base_url = app.config["NEWS_HIGHLIGHTS_BASE_URL"]
+
+# Getting the sources base url
+sources_base_url = app.config["NEWS_SOURCE_BASE_URL"]
+
+# Getting the entertainment news base url
+entertainment_base_url = app.config["NEWS_ENTERTAINMENT_BASE_URL"]
