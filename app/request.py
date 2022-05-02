2a060c23e6181e888e321601d0f0f1167d627eb4
edah-hub <edah.chepngetich@student.moringaschool.comm>
2022-05-02 16:05:06 +0300

    create a get_movies() function

--- app/request.py
+++ app/request.py
@@ -18,3 +18,23 @@ sources_base_url = app.config["NEWS_SOURCE_BASE_URL"]
 
 # Getting the entertainment news base url
 entertainment_base_url = app.config["NEWS_ENTERTAINMENT_BASE_URL"]
+
+
+def get_news(country):
+	'''
+	Function that gets the json response to our url request
+	'''
+	get_news_url = news_base_url.format(country,api_key)
+
+	with urllib.request.urlopen(get_news_url) as url:
+		get_news_data = url.read()
+		get_news_response = json.loads(get_news_data)
+
+		news_results = None
+
+		if get_news_response['articles']:
+			news_result_list = get_news_response['articles']
+			news_results = process_newsResults(news_result_list)
+
+
+	return news_results
