613cc6187e82bdef1e36126fdfc40088cd7341ce
edah-hub <edah.chepngetich@student.moringaschool.comm>
2022-05-03 15:33:29 +0300

    final updates

--- app/__init__.py
+++ app/__init__.py
@@ -12,3 +12,4 @@ app.config.from_object(DevConfig)
 app.config.from_pyfile('config.py')
 
 from app import views
+from app import error
--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from flask import render_template
 from app import app
-
+from .request import get_news,get_sources,getEntertainment
 # Views
 @app.route('/')
 def index():
@@ -8,4 +8,11 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    return render_template('index.html')
+    title = 'Home - News Highlight'
+    country_news = get_news('us')
+    news_source = get_sources()
+    entertainment_news = getEntertainment('entertainment')
+   
+    return render_template('index.html', title = title, country = country_news, sources = news_source, entertainment = entertainment_news)
+    
+   
